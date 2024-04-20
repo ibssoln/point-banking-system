8ec93a0efa683766213d6b366d9912a7601db05e
ibssoln <itbisys.system@gmail.com>
2024-04-19 20:46:00 -0400

    init

---
+++ src/main/java/ca/itbisys/pointbankingsystem/PointBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package ca.itbisys.pointbankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/itbisys/pointbankingsystem/PointBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.itbisys.pointbankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
